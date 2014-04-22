202c516201b8102959ab440ad22154e020fe1ed1
Rohit Sinha <talktorohit54@gmail.com>
2014-04-22 02:15:38 -0500

    DS: Implementation of Stack

---
+++ algorithms/stack.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""stack.py: Stack implementation"""
+
+__author__ = 'rohitsinha'
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items).strip('[]')
+
+
+s = Stack()
+
+print(s.isEmpty())
+s.push(5)
+s.push('Hello')
+print(s.peek())
+s.push(True)
+print(s.peek())
+print(s.size())
+print(s.pop())
+print(s)
